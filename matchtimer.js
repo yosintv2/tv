a22218b7c3dd6fffe231d5429093e8b38dc44ffd
yosintv2 <156112254+yosintv2@users.noreply.github.com>
2024-06-09 04:13:05 +0900

    Update matchtimer.js

--- matchtimer.js
+++ matchtimer.js
@@ -1,6 +1,6 @@
 (function() {
     // The domains you want to allow
-    const allowedDomains = [ "yosintv9999.blogspot.com"];
+    const allowedDomains = ["youtube.com", "example.com", "anotherdomain.com"];
 
     // Get the current URL
     const currentUrl = window.location.hostname;
@@ -8,8 +8,8 @@
     // Check if the current URL matches any of the allowed domains
     const isAllowed = allowedDomains.some(domain => currentUrl === domain || currentUrl === `www.${domain}`);
 
-    // If the current URL is not allowed, redirect to the main allowed domain
+    // If the current URL is not allowed, redirect to google.com
     if (!isAllowed) {
-        window.location.href = `https://${allowedDomain}`;
+        window.location.href = "https://www.google.com";
     }
 })();
